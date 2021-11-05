6e25bf0f50b12ed1a2e25dc657b70068741221ce
veekthooor360 <nwoguuchenna88@gmail.com>
2021-11-04 20:16:36 -0500

    5th

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@ $(function() {
                 // Only prevent default if animation is actually gonna happen
                 event.preventDefault();
                 $('html, body').animate({
-                    scrollTop: target.offset().top
+                   
                 }, 1000, function() {
                     // Callback after animation
                     // Must change focus!
